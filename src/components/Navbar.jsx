8a382591500b865723df33ca21a1b8764b016dca
sampath1532 <srisampath24@gmail.com>
2025-05-29 15:48:19 +0530

    project initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,193 @@
+import * as React from 'react';
+import { extendTheme, styled } from '@mui/material/styles';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import DescriptionIcon from '@mui/icons-material/Description';
+import LayersIcon from '@mui/icons-material/Layers';
+import { AppProvider } from '@toolpad/core/AppProvider';
+import { DashboardLayout } from '@toolpad/core/DashboardLayout';
+import { PageContainer } from '@toolpad/core/PageContainer';
+import Grid from '@mui/material/Grid2';
+import { Typography } from '@mui/material';
+import { useState } from 'react';
+import Dashboard from './dashboard';
+
+const NAVIGATION = [
+  {
+    kind: 'header',
+    title: 'Main items',
+  },
+  {
+    segment: 'dashboard',
+    title: 'Dashboard',
+    icon: <DashboardIcon />,
+  },
+  {
+    segment: 'orders',
+    title: 'Orders',
+    icon: <ShoppingCartIcon />,
+  },
+  {
+    kind: 'divider',
+  },
+  {
+    kind: 'header',
+    title: 'Analytics',
+  },
+  {
+    segment: 'reports',
+    title: 'Reports',
+    icon: <BarChartIcon />,
+    children: [
+      {
+        segment: 'sales',
+        title: 'Sales',
+        icon: <DescriptionIcon />,
+      },
+      {
+        segment: 'traffic',
+        title: 'Traffic',
+        icon: <DescriptionIcon />,
+      },
+    ],
+  },
+  {
+    segment: 'integrations',
+    title: 'Integrations',
+    icon: <LayersIcon />,
+  },
+];
+
+const demoTheme = extendTheme({
+  // colorSchemes: { light: true, dark: true },
+  colorSchemeSelector: "class",
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900, // Fixed overlapping breakpoint
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: {
+          main: "#1ac433", // Change this to your custom color
+        },
+      },
+    },
+    dark: {
+      palette: {
+        primary: {
+          main: "#cc1860",
+        },
+      },
+    },
+  },
+  components: {
+    MuiListItemButton: {
+      styleOverrides: {
+        root: {
+          "&.Mui-selected": {
+            backgroundColor: "#ff5722", // Selected background color
+            fontWeight: "blod",
+            borderRadius: "8px", // Optional rounded corners,
+          },
+          "&.Mui-selected:hover": {
+            backgroundColor: "#ff784e", // Hover color when selected
+          },
+        },
+      },
+    },
+  }
+});
+
+function DynamicComponentLoad({ pathname }){
+  console.log(pathname, "{ pathname }")
+  if(pathname === '/dashboard') return <Dashboard/>
+  else return(
+    <h1>welcome to static page</h1>
+  )
+}
+
+function useDemoRouter(initialPath) {
+  const [pathname, setPathname] = React.useState(initialPath);
+
+  const router = React.useMemo(() => {
+    return {
+      pathname,
+      searchParams: new URLSearchParams(),
+      navigate: (path) => setPathname(String(path)),
+    };
+  }, [pathname]);
+  console.log(router, "routerrouterrouter")
+  return router;
+}
+
+const Skeleton = styled('div')(({ theme, height }) => ({
+  backgroundColor: theme.palette.action.hover,
+  borderRadius: theme.shape.borderRadius,
+  height,
+  content: '" "',
+}));
+
+export default function DashboardLayoutBasic(props) {
+  // const { window } = props;
+
+  const router = useDemoRouter('/dashboard');
+
+  const [session, setSession] = useState({
+    user: {
+      name: 'Bharat Kashyap',
+      email: 'bharatkashyap@outlook.com',
+      image: 'https://avatars.githubusercontent.com/u/19550456',
+    },
+  });
+
+  const authentication = React.useMemo(() => {
+    return {
+      signIn: () => {
+        setSession({
+          user: {
+            name: 'Bharat Kashyap',
+            email: 'bharatkashyap@outlook.com',
+            image: 'https://avatars.githubusercontent.com/u/19550456',
+          },
+        });
+      },
+      signOut: () => {
+        setSession(null);
+      },
+    };
+  }, []);
+
+  // Remove this const when copying and pasting into your project.
+  // const demoWindow = window ? window() : undefined;
+
+  return (
+    <AppProvider
+      session={session}
+      authentication={authentication}
+      navigation={NAVIGATION}
+      router={router}
+      theme={demoTheme}
+      branding={{
+        logo: "",
+        // logo: <img src="https://mui.com/static/logo.png" alt="MUI logo" />,
+        title: 'User Management',
+        homeUrl: '/toolpad/core/introduction',
+      }}
+      // window={demoWindow}
+    >
+      <DashboardLayout sx={{ bgcolor: '#f1f5f9'}}>
+        <PageContainer breadcrumbs={[]} className="bg-slate-100">
+          {/* <Typography>hello world</Typography> */}
+          <DynamicComponentLoad pathname = { router.pathname }> </DynamicComponentLoad>
+        </PageContainer>
+      </DashboardLayout>
+    </AppProvider>
+  );
+}
---
+++ src/components/dashboard.jsx
@@ -0,0 +1,55 @@
+import { Box, Card, CardActionArea, CardContent, Grid2, Typography } from '@mui/material';
+import * as React from 'react'
+
+function Dashboard(){
+    const cards = [
+        {
+          id: 1,
+          title: 'Admin',
+          description: 5,
+        },
+        {
+          id: 2,
+          title: 'Users',
+          description: 10,
+        },
+        {
+          id: 3,
+          title: 'Consumers',
+          description: 20,
+        },
+    ];
+
+    return(
+        <Box className="bg-slate-100 flex flex-wrap justify-around gap-4 p-4">
+            {cards.map((card, index) => (
+                <Card 
+                    key={index}
+                    className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4"
+                    sx={{ 
+                        maxWidth: 300,
+                        background: "linear-gradient(to bottom, #6EE7B7, #3B82F6)", // Green to Blue
+                        color: "white", // Text color }} // Controls card width
+                    }}
+                >
+                    <CardActionArea
+                        sx={{
+                            height: '100%',
+                        }}
+                    >
+                        <CardContent sx={{ height: '100%' }} className='text-center'>
+                        <Typography variant="h5" component="div">
+                            {card.title}
+                        </Typography>
+                        <Typography variant="h5" color="text.secondary">
+                            {card.description}
+                        </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                </Card>
+            ))}
+        </Box>
+    )
+}
+
+export default Dashboard;
